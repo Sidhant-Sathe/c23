1fff5ccd3f4666dbd1f85bf1f44dfd9d2463228c
Sidhant-Sathe <83119127+Sidhant-Sathe@users.noreply.github.com>
2021-05-17 17:13:23 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,17 @@
+class Ground{
+    constructer(x,y,width,height){
+var options = {
+    isStatic:true
+}
+this.body=Bodies.rectangle(x,y,width,height,this.options);
+this.width=width;
+this.height=height;
+World.add(world,this.body);
+    }
+display(){
+var position = this.body.position;
+rectMode(CENTER);
+rect(position.x,position.y,this.width,this.height);
+}
+}
+
